d2c4694bcdef70a7ba27e9d4242a545211c524ff
sdcabral <sdcabral@sefaz.al.gov.br>
2022-11-03 09:51:19 -0300

    criação de um component

--- projectAngular/src/app/app.module.ts
+++ projectAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectAngularComponent } from './project-angular/project-angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectAngularComponent
   ],
   imports: [
     BrowserModule,
---
+++ projectAngular/src/app/project-angular/project-angular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-angular',
+  templateUrl: './project-angular.component.html',
+  styleUrls: ['./project-angular.component.css']
+})
+export class ProjectAngularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
