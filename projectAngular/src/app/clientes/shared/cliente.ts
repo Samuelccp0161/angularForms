b425fe4a38ae206588a4419fae6888ee655a671b
sdcabral <sdcabral@sefaz.al.gov.br>
2022-11-04 13:54:21 -0300

    inteface V 0.001, feractorar e ajeitar os botões

---
+++ projectAngular/src/app/clientes/shared/cliente.ts
@@ -0,0 +1,23 @@
+export class Cliente {
+    login: string = '';
+    password: string = '';
+    contaGrafica: string='';
+    tipo: number = 1;
+    genero: string = "1";
+    dataNascimento: Date = new Date();
+    observacao: string = '';
+    inativo: boolean = false;
+  }
+
+/*
+O que eu preciso fazer:
+
+Login:(String)
+Senha:(String anonima)
+
+numero conta grafica:(String so numero)
+
+import pdf.
+
+button submit.
+*/
